6b7c21dc4997c8bbd0b8008e475ffd47f3aa1d99
Eshwar Chandra <eshwarcc@gmail.com>
2012-04-25 00:56:51 +0530

    d1. dimi1, dime1, dims1, inti1, inte1, ints1 added to result one, 2. added totals of dimi1, dime1, dims1, inti1, inte1, ints1 and 3. Created models functions for the same and controller functions for calculating totals

--- app/models/testone.rb
+++ app/models/testone.rb
@@ -1,2 +1,34 @@
 class Testone < ActiveRecord::Base
+
+	def dim1 new_row_number
+		(self.row_number - new_row_number.to_i).abs
+	end
+
+	def dim_i1 new_row_number
+		self.group_code == 'I' ? self.dim1(new_row_number) : '-'
+	end
+
+	def dim_e1 new_row_number
+		self.group_code == 'E' ? self.dim1(new_row_number) : '-'
+	end
+
+	def dim_s1 new_row_number
+		self.group_code == 'S' ? self.dim1(new_row_number) : '-'
+	end
+
+	def int_i1 new_row_number
+		int1 = self.dim1(new_row_number) - 2
+		return ((self.group_code == 'I') and (int1 > 0)) ? int1 : 0
+	end
+
+	def int_e1 new_row_number
+		int1 = self.dim1(new_row_number) - 2
+		return ((self.group_code == 'E') and (int1 > 0)) ? int1 : 0
+	end
+
+	def int_s1 new_row_number
+		int1 = self.dim1(new_row_number) - 2
+		return ((self.group_code == 'S') and (int1 > 0)) ? int1 : 0
+	end
+
 end
